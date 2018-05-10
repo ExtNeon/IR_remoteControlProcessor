2f7f7a33e113bc10ca1e372463dee10c5c52fef4
Кирилл Малякин <malyakin-10@yandex.ru>
2018-05-10 13:32:38 +0300

    Написаны комментарии.

--- src/utils/ConsoleUtils.java
+++ src/utils/ConsoleUtils.java
@@ -4,15 +4,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Created by Кирилл on 26.03.2018.
+ * Сборка утилит для работы с вводом - выводом из/в коммандную строку.
  */
 public class ConsoleUtils {
 
+    /**
+     * Выводит пользователю пояняющее сообщение, и возвращает введённую им строку.
+     *
+     * @param message Сообщение, которое будет выведено.
+     * @return Введённая пользователем строка
+     */
     public static String getEnteredString(String message) {
         System.out.print(message);
         return new Scanner(System.in).nextLine();
     }
 
+    /**
+     * Выводит пользователю пояняющее сообщение, и возвращает введённое им целое число.
+     * @param message Сообщение, которое будет выведено.
+     * @return Введённое пользователем число.
+     */
     public static int getEnteredIntegerNumber(String message) {
         System.out.print(message);
         for (; ; ) {
@@ -26,7 +37,6 @@ public class ConsoleUtils {
 
     /**
      * Данный метод выводит строку в консоль, не мешая вводу пользователя (теоретически)
-     *
      * @param text Текст, что будет выведен в консоль
      */
     public static void printToConsole(String text) {
